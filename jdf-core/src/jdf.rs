f9d30f31e941a701fea8b2a968db1a5baf835408
yutapok <yutautsugi@outlook.com>
2021-06-24 10:45:50 +0900

    modify: parse json string as Map

--- jdf-core/src/jdf.rs
+++ jdf-core/src/jdf.rs
@@ -1,4 +1,4 @@
-use serde_json::{Value, Map};
+use serde_json::{json, Value, Map};
 
 pub struct Jdf {
     pub value: Value
@@ -39,6 +39,16 @@ fn json_flatten(key: String, value: Value) -> Map<String, Value> {
             .map(|(i, v)| json_flatten(format!("{}.[{}]", key, i), v.clone()))
             .flat_map(|fm| fm)
             .collect()
+        },
+        Value::String(s) => {
+            let v: Value = serde_json::from_str(&s).unwrap_or(Value::Null);
+            if v.is_object(){
+                json_flatten(key.clone(), v)
+            } else {
+                let mut mp = Map::new();
+                mp.insert(key, json!(s));
+                mp
+            }
         },
         others => {
             let mut mp = Map::new();
